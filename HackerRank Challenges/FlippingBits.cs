06ae96b5d37d2e38901cef2c5990ea95b9bd4d24
Charuta Joshi <Charuta.Joshi@se2.com>
2023-09-01 17:30:56 -0500

    Started new challenge

---
+++ HackerRank Challenges/FlippingBits.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.HackerRank_Challenges
+{
+    /// <summary>
+    /// You will be given a list of 32 bit unsigned integers. Flip all the bits (1->0 and 0->1) and return the result as an unsigned integer.
+    /// </summary>
+    public class FlippingBits
+    {
+        public static long flippingBits(long n)
+        {
+            var nBinary = Convert.ToString(n, 2);
+            Console.WriteLine(nBinary); 
+            char[] chars = nBinary.ToCharArray();
+
+            foreach (var c in chars)
+            {
+                IEnumerable<char> bytes = IEnumerable;  
+                char bit = 'a';
+
+                if (c == '0')
+                {
+                    bit = '1'; 
+                }
+                else
+                {
+                    bit = '0'; 
+                }
+            }
+
+            return new long(); 
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -19,7 +19,7 @@ namespace ConsoleApp1
     {
         public static void Main(string[] args)
         {
-            CallLonelyInteger(); 
+            CallFlippingBits(); 
         }
 
         public static void CallSparseArrays()
@@ -57,6 +57,25 @@ namespace ConsoleApp1
             int result = LonelyInteger.lonelyinteger(a);
 
             Console.WriteLine(result);
+        }
+
+        public static void CallFlippingBits()
+        {
+            TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            int q = Convert.ToInt32(Console.ReadLine().Trim());
+
+            for (int qItr = 0; qItr < q; qItr++)
+            {
+                long n = Convert.ToInt64(Console.ReadLine().Trim());
+
+                long result = FlippingBits.flippingBits(n);
+
+                textWriter.WriteLine(result);
+            }
+
+            textWriter.Flush();
+            textWriter.Close();
         }
     }
 }
