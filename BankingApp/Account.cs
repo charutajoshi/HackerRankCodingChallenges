93e2ef619758f0a523bd30d4a5ae8d54b88a3af1
charutaj <charuta1.joshi@gmail.com>
2023-09-10 17:45:12 -0500

    Started work on banking app

---
+++ BankingApp/Account.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace ConsoleApp1.BankingApp
+{
+	public class Account
+	{
+		public Account()
+		{
+		}
+
+		public string ID { get; set; }
+		public double Amount { get; set; }
+		public List<DateTime> TimeStamp { get; set; }
+    }
+}
+
---
+++ BankingApp/BankingApp.cs
@@ -0,0 +1,47 @@
+﻿using System;
+namespace ConsoleApp1.BankingApp
+{
+	public class BankingApp
+	{
+		private List<Account> accounts = new List<Account>();
+
+		public BankingApp()
+		{
+		}
+
+		public void CreateAccount(string acctID, DateTime time)
+		{
+			List<string> ids = new List<string>();
+
+			foreach (var acct in accounts)
+			{
+				ids.Add(acct.ID); 
+			}
+
+			if (!ids.Contains(acctID))
+			{
+				Account newAcct = new Account();
+				newAcct.ID = acctID;
+				List<DateTime> dateTimes = new List<DateTime>();
+				dateTimes.Add(time);
+				newAcct.TimeStamp = dateTimes; 
+				Console.WriteLine($"Account {acctID} created.");
+            }
+            else
+			{
+				Console.WriteLine($"Account {acctID} already exists."); 
+			}
+		}
+
+		public void Deposit(string acctID, DateTime time, double amount)
+		{
+
+		}
+
+		public void Payment(string acctID, DateTime time, double amount)
+		{
+
+		}
+	}
+}
+
